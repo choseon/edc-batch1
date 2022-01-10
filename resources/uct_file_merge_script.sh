81d273ac97b8db25be868e5e99f1a00ae09df8ab
choseon <choseon1103@naver.com>
2022-01-10 10:15:36 +0900

    최종 소스

--- resources/uct_file_merge_script.sh
+++ resources/uct_file_merge_script.sh
@@ -1,18 +1,18 @@
-ls C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_1*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_1.txt
-rm -rf C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_1*_*
-ls C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_2*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_2.txt
-rm -rf C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_2*_*
-ls C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_3*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_3.txt
-rm -rf C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_3*_*
-ls C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_4*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_4.txt
-rm -rf C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_4*_*
-ls C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_5*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_5.txt
-rm -rf C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_5*_*
-ls C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_6*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_6.txt
-rm -rf C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_6*_*
-ls C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_7*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_7.txt
-rm -rf C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_7*_*
-ls C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_8*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_8.txt
-rm -rf C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_8*_*
-ls C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_9*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_9.txt
-rm -rf C:/dev/hdata/ht_uct001m/temp/2017/ht_uct001m_202112_2017_9*_*
+ls C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_1*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_1.txt
+rm -rf C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_1*_*
+ls C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_2*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_2.txt
+rm -rf C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_2*_*
+ls C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_3*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_3.txt
+rm -rf C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_3*_*
+ls C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_4*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_4.txt
+rm -rf C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_4*_*
+ls C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_5*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_5.txt
+rm -rf C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_5*_*
+ls C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_6*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_6.txt
+rm -rf C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_6*_*
+ls C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_7*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_7.txt
+rm -rf C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_7*_*
+ls C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_8*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_8.txt
+rm -rf C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_8*_*
+ls C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_9*_* | xargs cat > C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_9.txt
+rm -rf C:/dev/hdata/ht_uct001m/temp/2020/ht_uct001m_202201_2020_9*_*
